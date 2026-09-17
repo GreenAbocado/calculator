d06555cbb5de145cf66ad1cab560e3827ef64d44
GreenAbocado <dlwjdgus7038@gmail.com>
2026-09-17 18:24:20 +0900

    refactor: 예외 메시지 수정 츌력 형식 변경

--- src/exception/ExceptionHandler.java
+++ src/exception/ExceptionHandler.java
@@ -1,18 +1,27 @@
 package exception;
 
+import java.util.NoSuchElementException;
+
 public class ExceptionHandler {
     public static void handleException(Runnable logic) {
         try {
             logic.run();
         }
         catch (IllegalArgumentException e) {
-            System.out.println("[입력 값 예외]");
-            System.out.println(e.getMessage());
+            printError("[입력 값 예외]: ", e);
+        } catch (ArithmeticException e) {
+            printError("[계산 중 예외]: ", e);
+        } catch (NoSuchElementException e) {
+            printError("[저장소 관련 예외]: ", e);
         }
-        catch (ArithmeticException e) {
-            System.out.println("[계산 중 예외]");
-            System.out.println(e.getMessage());
-        }
+    }
+
+    private static void printError(String str, Exception e) {
+        System.out.println(
+                "===============================\n" +
+                        str + e.getMessage() + "\n" +
+                "==============================="
+        );
     }
 
     // 객체 생성 방지
