87b2f5d0e9b0aeb832a2109da0937f848dfab517
GreenAbocado <dlwjdgus7038@gmail.com>
2026-09-18 15:25:10 +0900

    refactor: 연산 책임Operator에 위임, Number를 통해 계산기 다형성 고려 설계

--- src/calculator/CalculateResult.java
+++ src/calculator/CalculateResult.java
@@ -1,25 +1,30 @@
 package calculator;
 
 public class CalculateResult {
-    private final long firstNum;
-    private final long secondNum;
-    private final char operator;
-    private final double result;
+    private final Number firstNum;
+    private final Number secondNum;
+    private final Operator operator;
+    private final Number result;
 
-    private CalculateResult(long firstNum, long secondNum, char operator, double result) {
+    private CalculateResult(Number firstNum, Number secondNum, Operator operator, Number result) {
         this.firstNum = firstNum;
         this.secondNum = secondNum;
         this.operator = operator;
         this.result = result;
     }
 
-    public static CalculateResult of (long firstNum, long secondNum, char operator, double result) {
+    public static CalculateResult of (Number firstNum, Number secondNum, Operator operator, Number result) {
         return new CalculateResult(firstNum, secondNum, operator, result);
     }
 
     @Override
     public String toString() {
-        // 자바가 문자열 연산 최적화
-        return "(" + firstNum + operator + secondNum + " = " + result + ")";
+        // 각 Number 구현체의 toString() 호출로 정수/실수 다르게 출력
+        return String.format("{ 피연산자1:%s, 피연산자2:%s, 연산자:%c, 결과:%s }\n",
+                firstNum, secondNum, operator.getType(), result);
+    }
+
+    public Number getResult() {
+        return result;
     }
 }
